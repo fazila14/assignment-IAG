b46c97dcd58e5fc968773f0889a36fa3b66d1710
Kwikl-Client <kwiklclient@gmail.com>
2025-11-02 12:59:26 +0530

    first commit

---
+++ ai-slide-builder/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ChatWindow from "./components/ChatWindow";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>AI Slide Builder</h1>
+      <ChatWindow />
+    </div>
+  );
+}
+
+export default App;
